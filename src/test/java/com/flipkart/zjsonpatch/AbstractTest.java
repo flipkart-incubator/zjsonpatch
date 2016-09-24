f9aef61424f57f25611640a7f8e7f46b42f27f49
Alexander Yastrebov <yastrebov.alex@gmail.com>
2016-09-24 10:13:57 +0200

    Fix error test cases (#22)
    * Refactored tests to use JUnit @Parameterized framework
    
    * Fix array out of bound error tests
    
    * Disabled test case A.13. Invalid JSON Patch Document as invalid json is handled by Jackson

--- src/test/java/com/flipkart/zjsonpatch/AbstractTest.java
+++ src/test/java/com/flipkart/zjsonpatch/AbstractTest.java
@@ -1,88 +1,54 @@
 package com.flipkart.zjsonpatch;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
-/**
- * @author ctranxuan (streamdata.io).
- */
+@RunWith(Parameterized.class)
 public abstract class AbstractTest {
-    ObjectMapper objectMapper = new ObjectMapper();
-    ArrayNode jsonNode;
-    ArrayNode errorNode;
 
-    protected AbstractTest(String fileName) throws IOException {
-        String path = "/testdata/" + fileName + ".json";
-        InputStream resourceAsStream = JsonDiffTest.class.getResourceAsStream(path);
-        String testData = IOUtils.toString(resourceAsStream, "UTF-8");
-        JsonNode testsNode = objectMapper.readTree(testData);
-        jsonNode = (ArrayNode) testsNode.get("ops");
-        errorNode = (ArrayNode) testsNode.get("errors");
-    }
+    @Parameter
+    public PatchTestCase p;
 
     @Test
-    public void testPatchAppliedCleanly() throws Exception {
-        for (int i = 0; i < jsonNode.size(); i++) {
-            JsonNode first = jsonNode.get(i).get("node");
-            JsonNode second = jsonNode.get(i).get("expected");
-            JsonNode patch = jsonNode.get(i).get("op");
-            String message = jsonNode.get(i).has("message") ? jsonNode.get(i).get("message").toString() : "";
-
-            System.out.println("Test # " + i);
-            System.out.println(first);
-            System.out.println(second);
-            System.out.println(patch);
-
-            JsonNode secondPrime = JsonPatch.apply(patch, first);
-            System.out.println(secondPrime);
-            Assert.assertThat(message, secondPrime, equalTo(second));
+    public void test() throws Exception {
+        if (p.isOperation()) {
+            testOpertaion();
+        } else {
+            testError();
         }
     }
 
-//    @Test
-//    public void testPatchSyntax() throws Exception {
-//        for (int i = 0; i < jsonNode.size(); i++) {
-//            JsonNode first = jsonNode.get(i).get("node");
-//            JsonNode second = jsonNode.get(i).get("expected");
-//            JsonNode patch = jsonNode.get(i).get("op");
-//
-//            System.out.println("Test # " + i);
-//            System.out.println(first);
-//            System.out.println(second);
-//            System.out.println(patch);
-//
-//            JsonNode diff = JsonDiff.asJson(first, second);
-//            System.out.println(diff);
-//            Assert.assertThat(diff, equalTo(patch));
-//        }
-//    }
+    private void testOpertaion() throws Exception {
+        JsonNode node = p.getNode();
 
-    @Test(expected = JsonPatchApplicationException.class)
-    public void testErrorsAreCorrectlyReported() {
-        for (int i = 0; i < errorNode.size(); i++) {
-            JsonNode first = errorNode.get(i).get("node");
-            JsonNode patch = errorNode.get(i).get("op");
+        JsonNode first = node.get("node");
+        JsonNode second = node.get("expected");
+        JsonNode patch = node.get("op");
+        String message = node.has("message") ? node.get("message").toString() : "";
 
-            System.out.println("Error Test # " + i);
-            System.out.println(first);
-            System.out.println(patch);
+        JsonNode secondPrime = JsonPatch.apply(patch, first);
 
-            JsonNode secondPrime = JsonPatch.apply(patch, first);
-            System.out.println(secondPrime);
-        }
-
-        if (errorNode.size() == 0) {
-            throw new JsonPatchApplicationException("dummy exception");
-        }
+        assertThat(message, secondPrime, equalTo(second));
     }
 
+    private void testError() {
+        JsonNode node = p.getNode();
+
+        JsonNode first = node.get("node");
+        JsonNode patch = node.get("op");
+
+        try {
+            JsonPatch.apply(patch, first);
+
+            fail("Failure expected: " + node.get("message"));
+        } catch (Exception ex) {
+        }
+    }
 }
--- src/test/java/com/flipkart/zjsonpatch/RemoveOperationTest.java
+++ src/test/java/com/flipkart/zjsonpatch/RemoveOperationTest.java
@@ -1,13 +1,16 @@
 package com.flipkart.zjsonpatch;
 
 import java.io.IOException;
+import java.util.Collection;
+import org.junit.runners.Parameterized;
 
 /**
  * @author ctranxuan (streamdata.io).
  */
 public class RemoveOperationTest extends AbstractTest {
 
-    public RemoveOperationTest() throws IOException {
-        super("remove");
+    @Parameterized.Parameters
+    public static Collection<PatchTestCase> data() throws IOException {
+        return PatchTestCase.load("remove");
     }
 }
