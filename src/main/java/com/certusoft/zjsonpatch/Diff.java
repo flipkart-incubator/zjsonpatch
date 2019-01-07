9c7e1ae688fd40bab0928d2dfc763657e6ed4373
Chris Mueller <chris.mueller@certusoft.com>
2019-01-07 12:53:43 -0600

    Rename Package

---
+++ src/main/java/com/certusoft/zjsonpatch/Diff.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016 flipkart.com zjsonpatch.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.certusoft.zjsonpatch;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
+/**
+ * User: gopi.vishwakarma
+ * Date: 30/07/14
+ */
+class Diff {
+    private final Operation operation;
+    private final List<Object> path;
+    private final JsonNode value;
+    private List<Object> toPath; //only to be used in move operation
+    private final JsonNode srcValue; // only used in replace operation
+
+    Diff(Operation operation, List<Object> path, JsonNode value) {
+        this.operation = operation;
+        this.path = path;
+        this.value = value;
+        this.srcValue = null;
+    }
+
+    Diff(Operation operation, List<Object> fromPath, List<Object> toPath) {
+        this.operation = operation;
+        this.path = fromPath;
+        this.toPath = toPath;
+        this.value = null;
+        this.srcValue = null;
+    }
+    
+    Diff(Operation operation, List<Object> path, JsonNode srcValue, JsonNode value) {
+        this.operation = operation;
+        this.path = path;
+        this.value = value;
+        this.srcValue = srcValue;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public List<Object> getPath() {
+        return path;
+    }
+
+    public JsonNode getValue() {
+        return value;
+    }
+
+    public static Diff generateDiff(Operation replace, List<Object> path, JsonNode target) {
+        return new Diff(replace, path, target);
+    }
+    
+    public static Diff generateDiff(Operation replace, List<Object> path, JsonNode source, JsonNode target) {
+        return new Diff(replace, path, source, target);
+    }
+    
+    List<Object> getToPath() {
+        return toPath;
+    }
+    
+    public JsonNode getSrcValue(){
+        return srcValue;
+    }
+}
---
+++ src/main/java/com/certusoft/zjsonpatch/DiffFlags.java
@@ -0,0 +1,47 @@
+package com.certusoft.zjsonpatch;
+
+import java.util.EnumSet;
+
+public enum DiffFlags {
+    /**
+     * This flag omits the <i>value</i> field on remove operations.
+     * This is a default flag.
+     */
+    OMIT_VALUE_ON_REMOVE,
+
+    /**
+     * This flag omits all {@link Operation#MOVE} operations, leaving only
+     * {@link Operation#ADD}, {@link Operation#REMOVE}, {@link Operation#REPLACE}
+     * and {@link Operation#COPY} operations. In other words, without this flag,
+     * {@link Operation#ADD} and {@link Operation#REMOVE} operations are not normalized
+     * into {@link Operation#MOVE} operations.
+     */
+    OMIT_MOVE_OPERATION,
+
+    /**
+     * This flag omits all {@link Operation#COPY} operations, leaving only
+     * {@link Operation#ADD}, {@link Operation#REMOVE}, {@link Operation#REPLACE}
+     * and {@link Operation#MOVE} operations. In other words, without this flag,
+     * {@link Operation#ADD} operations are not normalized into {@link Operation#COPY}
+     * operations.
+     */
+    OMIT_COPY_OPERATION,
+
+    /**
+     * This flag adds a <i>fromValue</i> field to all {@link Operation#REPLACE}operations.
+     * <i>fromValue</i> represents the the value replaced by a {@link Operation#REPLACE}
+     * operation, in other words, the original value.
+     *
+     * @since 0.4.1
+     */
+    ADD_ORIGINAL_VALUE_ON_REPLACE;
+
+
+    public static EnumSet<DiffFlags> defaults() {
+        return EnumSet.of(OMIT_VALUE_ON_REMOVE);
+    }
+
+    public static EnumSet<DiffFlags> dontNormalizeOpIntoMoveAndCopy() {
+        return EnumSet.of(OMIT_MOVE_OPERATION, OMIT_COPY_OPERATION);
+    }
+}
