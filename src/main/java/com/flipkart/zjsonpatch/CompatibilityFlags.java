9c5b1f352ebc5ca890fbea2baae43cc604d137d9
mbolgariw <36600613+mbolgariw@users.noreply.github.com>
2019-07-20 11:02:31 +0300

    Add compatibility flag for ignoring missing source node in replace operation (#102)

--- src/main/java/com/flipkart/zjsonpatch/CompatibilityFlags.java
+++ src/main/java/com/flipkart/zjsonpatch/CompatibilityFlags.java
@@ -23,7 +23,8 @@ import java.util.EnumSet;
  */
 public enum CompatibilityFlags {
     MISSING_VALUES_AS_NULLS,
-    REMOVE_NONE_EXISTING_ARRAY_ELEMENT;
+    REMOVE_NONE_EXISTING_ARRAY_ELEMENT,
+    ALLOW_MISSING_TARGET_OBJECT_ON_REPLACE;
 
     public static EnumSet<CompatibilityFlags> defaults() {
         return EnumSet.noneOf(CompatibilityFlags.class);
